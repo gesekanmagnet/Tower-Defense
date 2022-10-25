203bb2b7a2347e5b2ab4211ee4c43f511d0898e3
gesekanmagnet <113659944+gesekanmagnet@users.noreply.github.com>
2022-10-25 13:33:39 +0700

    Add files via upload

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+
+	public static float coin = 300f;
+	public static bool isGameOver = false;
+	public static bool isComplete = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TowerDestroy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDestroy : MonoBehaviour {
+
+	public bool isPlayer = true;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy()
+	{
+		if(!Data.isGameOver)
+		{
+			if(isPlayer)
+			{
+				Data.isGameOver = true;
+				Data.isComplete = false;
+				Debug.Log("Player Lost");
+			}
+			else
+			{
+				Data.isGameOver = true;
+				Data.isComplete = true;
+				Debug.Log("Player Win");
+			}
+		}
+	}
+}
